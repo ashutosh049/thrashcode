157fd89a447b031412cc6b02e4877f27cc355420
Kumar Ashutosh Dubey <ashutosh049@outlook.com>
2016-06-22 01:52:24 +0530

    Constant Utility Class.

---
+++ src/main/java/util/ThrashConstants.java
@@ -0,0 +1,23 @@
+package util;
+
+public class ThrashConstants {
+
+	public static String ARTICLE_ID = "artcl_id";
+	public static String ARTICLE_DATE = "artcl_date";
+	public static String ARTICLE_OWNER_NAME = "artcl_owner_name";
+	public static String ARTICLE_TAGS = "artcl_tags";
+	public static String ARTICLE_OWNER_ID = "artcl_owner_id";
+	public static String ARTICLE_TITLE = "artcle_title";
+	public static String ARTICLE_BRIEF_DESCRIPTION = "artcle_brf_desc";
+	public static String ARTICLE_DATA = "artcle_Data";
+	public static String ARTICLE_FILE_PATH = "artcle_filePath";
+	public static String ARTICLE_LAST_UPDATE_DATE = "artcle_lastUpdateDate";
+	public static String ARTICLE_HITS = "artcle_hits";
+	public static String ARTICLE_TYPE = "artcle_type";
+	public static String ARTICLE_CATEGORY_ID = "artcl_cat_id";
+	public static String PUBLISHARTICLE_TITLE = "publishArticle_title";
+	public static String PUBLISHARTICLE_ARTCLTYPES = "publishArticle_artcltypes";
+	public static String PUBLISHARTICLE_DESC = "publishArticle_desc";
+	public static String PUBLISHARTICLE_TAGS = "publishArticle_tags";
+	public static String CKEDITORDATA = "ckEditorData";
+}
